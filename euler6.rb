722a7e2cdc9f4d55ec22ba261e2b5e6b7f16992b
Brian <riceb53@gmail.com>
2018-10-21 09:27:38 -0500

    solve sum of the squares

---
+++ euler6.rb
@@ -0,0 +1,37 @@
+# very first thing you should do
+# make sure you understand the question/problem, solve it by hand
+
+# sum of the squares
+# (1 * 1) + (2 * 2) + (3 * 3) + (4 * 4) ... (10 * 10)
+
+# start with the number 1 -- DONE
+# increase that number by 1, some amount of times (probably a loop) -- DONE
+# figure out how to square a number -- DONE
+# add the numbers together (SUM)
+
+sum = 0
+index = 1
+10.times do
+  sum += index * index
+  index += 1
+end
+p sum
+
+
+# square of the sum
+# (1 + 2 + 3 + 4) ** 2
+
+# subtract the two numbers
+
+
+
+
+# The sum of the squares of the first ten natural numbers is,
+
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
